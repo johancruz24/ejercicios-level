33d3650fa5133f2682e4d942b1c539f8023cb569
johancruz24 <62783655+johancruz24@users.noreply.github.com>
2020-10-24 13:12:14 -0500

    Change Port

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan')
 const app = express();
 
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false }))
