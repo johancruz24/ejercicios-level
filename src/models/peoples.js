e67cb3783e1d0cbbb08f242b713e1b75ef6962ae
johancruz24 <62783655+johancruz24@users.noreply.github.com>
2020-10-24 13:02:05 -0500

    Change warning message

--- src/models/peoples.js
+++ src/models/peoples.js
@@ -4,9 +4,9 @@ var  emailCompare = [/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
 var  nameCompare = [/^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/,"Must be Pepito Perez"];
 
 const peopleSchema = new Schema({
-    name: {type: String, required: "Name is required ",minlength:[5,"Name must have 8 character"], match: nameCompare},
-    mail: {type: String, required: "Email is required ", match: emailCompare, },
-    km: {type: Number, min:[4,"you must walk more "], required: "you must enter a distance"}
+    name: {type: String, required: "The name field is required ",minlength:[5,"Name must have 8 character"], match: nameCompare},
+    mail: {type: String, required: "The email field is required ", match: emailCompare, },
+    km: {type: Number, min:[4,"Lets go !!! You can walk more "], required: "The distance field is required"}
 }, {
     timestamps: true, 
     versionKey: false
